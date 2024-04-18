60b10bdf31fa6101c7f2aa242d962d00db50f3ec
Kartik Kochhar <kochharkartik51@gmail.com>
2024-04-18 16:17:45 +0530

    created Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+const Navbar = () => {
+  return (
+    <div className="flex justify-between py-4 px-10"> Navbar
+
+
+        <ul className="flex gap-5 cursor-pointer ">
+            <li>Home</li>
+            <li>About</li>
+            <li>Acaedemics</li>
+            <li>Community</li>
+        </ul>
+
+    </div>
+  )
+}
+
+export default Navbar
