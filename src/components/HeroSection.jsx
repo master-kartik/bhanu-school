c2e0b4012d2009b760d8313b15343ef227b2211a
Kartik Kochhar <kochharkartik51@gmail.com>
2024-05-13 07:23:33 +0530

    deployed

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const HeroSection = ({title,decription}) => {
   return (
     <div className='flex flex-col w-full my-10 px-10 items-center'>
-        <div className='text-5xl tracking-tighter font-normal text-[#111C2C]'>{title}</div>
+        <div className='text-5xl tracking-tighter text-center font-normal text-[#111C2C]'>{title}</div>
         <div className='text-md my-4 w-2/3 text-[#9C9998] tracking-tight text-center'>{decription}
 </div>
     </div>
