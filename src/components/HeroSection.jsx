26456421f97700b4a123b507572b26cf8acee0af
Kartik Kochhar <kochharkartik51@gmail.com>
2024-05-17 20:06:10 +0530

    ui fixes v2

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const HeroSection = ({title,decription}) => {
   return (
-    <div className='flex flex-col w-full my-10 px-10 text-center items-center'>
+    <div className='flex flex-col w-full my-10 px-5 lg:px-10 text-center items-center'>
         <div className='text-5xl tracking-tighter justify-center text-center font-normal text-[#111C2C]'>{title}</div>
-        <div className='text-md my-4 w-2/3 text-[#9C9998] tracking-tight text-center'>{decription}
+        <div className='text-md my-4 lg:w-2/3 text-[#9C9998] tracking-tight text-left lg:text-center'>{decription}
 </div>
     </div>
   )
