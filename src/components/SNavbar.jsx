66046df68016fc7ba1850882b6f62267cbf0254d
Kartik Kochhar <kochharkartik51@gmail.com>
2024-05-19 09:47:10 +0530

    navbar

--- src/components/SNavbar.jsx
+++ src/components/SNavbar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import '../index.css'
 import { NavLink } from "react-router-dom"
+
 import {
   Collapse,
   IconButton,
@@ -20,12 +21,15 @@ export default function SNavbar() {
   }, []);
  
   const navList = (
-    <div className={` md:flex w-auto md:bg-transparent h-screen md:h-auto text-center md:text-left  md:gap-5 items-center z-50 p-16 md:p-0 `}> 
+    <div className={` md:flex w-auto md:bg-transparent mb-0 h-screen md:h-auto text-center md:text-left  md:gap-5 items-center z-50 p-16 md:p-0 `}> 
     {["About", "Academics", "Contact","Search"].map((data, index)=>(
 
-      index === 3 ? (<i key={{index}} className="ri-search-line text-background md:text-text text-3xl  md:ml-6 cursor-pointer md:text-lg " ></i>) : (
+      index === 3 ? (<div className="flex  items-center">
+        <input type="text" placeholder="Search" className="md:hidden input re input-bordered rounded-lg w-[50vw] m-8 md:m-0 md:w-[10vw] text-sm md:h-[5vh] h-[5vh] max-w-xs" />
+        <i key={{index}} className=" ri-search-line text-background md:text-text text-3xl  md:ml-2 cursor-pointer md:text-lg " ></i>
+      </div>) : (
   
-        <NavLink key={{index}}  to={`/${data.toLocaleLowerCase()}`} ><div className=" text-3xl hover:bg-dark hover:shadow-lg p-6 rounded-lg shadow-sm md:mb-0 font-bold tracking-tighter md:text-sm">{data}</div></NavLink>
+        <NavLink key={{index}}  to={`/${data.toLocaleLowerCase()}`} ><div className=" text-3xl hover:bg-dark hover:shadow-lg  md:hover:bg-transparent md:shadow-none md:hover:shadow-none p-6 md:p-0 rounded-lg shadow-sm md:mb-0 font-bold md:font-semibold tracking-tighter md:text-sm">{data}</div></NavLink>
       )
     )
 
@@ -45,13 +49,13 @@ export default function SNavbar() {
         <NavLink to={'/'}  className="flex -gap-1 items-center text-lg select-none tracking-tighter cursor-pointer font-bold z-10">Bhanu School</NavLink>
         
           <div className="flex items-center justify-between gap-4">
-            <div className="hidden lg:block">
+            <div className="hidden md:block">
                 {navList}
                 
             </div>
             <IconButton
               variant="text"
-              className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+              className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent md:hidden"
               ripple={false}
               onClick={() => setOpenNav(!openNav)}
             >
@@ -88,7 +92,7 @@ export default function SNavbar() {
             </IconButton>
           </div>
         
-       <div className="md:hidden absolute top-16 z-30 w-full w-screen-md mx-auto  left-1/2  transform -translate-x-1/2 text-background bg-text bg-opacity-[99%] backdrop-blur-lg  rounded-lg  shadow-md">
+       <div className="md:hidden absolute top-[60px] z-30 w-full w-screen-md mx-auto  left-1/2  transform -translate-x-1/2 text-background bg-text  bg-opacity-[99%] backdrop-blur-lg  rounded-lg  shadow-md">
        <Collapse  className="" open={openNav}>
           
           
