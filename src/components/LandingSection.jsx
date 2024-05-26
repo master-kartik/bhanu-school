f5b09cf460203805a806a86054884eef51e48d94
Kartik Kochhar <kochharkartik51@gmail.com>
2024-05-27 00:40:09 +0530

    minors

--- src/components/LandingSection.jsx
+++ src/components/LandingSection.jsx
@@ -6,8 +6,8 @@ const LandingSection = () => {
         src="https://www.stirworld.com/images/article_gallery/the-gyaan-centre-jaisalmer-designed-by-diana-kellogg-with-the-citta-organisation-rajkumari-ratnavati-school-and-women-s-center-diana-kellog-architects-stirworld-210125050245_t.jpg"
         alt="Bhanu Public School"
       />
-      <div className="absolute top-3/4 md:top-1/3 left-1/2 transform font-EditorialNew -translate-x-1/2 -translate-y-1/2 text-3xl lg:text-6xl text-[#f9f9f3] z-10 text-left md:text-center  leading-[0.6] lg:leading-[0.50] font-extralight select-none tracking-tight sm:leading-[0.5] w-[80vw] md:[70vw] lg:w-[60vw]">
-        Bhanu Public School{" "} <br/><span className="font-Archivo font-thin mt-0 text-xl md:text-2xl tracking-normal text-[#f9f9f3c0]">bridging education</span>
+      <div className="absolute top-3/4 md:top-1/3 left-1/2 transform font-EditorialNew -translate-x-1/2 -translate-y-1/2 text-2xl md:3xl lg:text-6xl text-[#f9f9f3] z-10 text-left md:text-center  leading-[0.6] lg:leading-[0.50] font-extralight select-none tracking-tight sm:leading-[0.5] w-[80vw] md:[70vw] lg:w-[60vw]">
+        Bhanu Public School{" "} <br/><span className="font-Archivo font-thin mt-0 text-lg md:text-2xl tracking-normal text-[#f9f9f3c0]">bridging education</span>
       </div>
     </div>
   );
